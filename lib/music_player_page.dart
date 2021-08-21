cd2d4a51b07b11bbfe405f42e30e460c16ab2c58
ashndev <87834445+ashndev@users.noreply.github.com>
2021-08-20 22:02:47 -0700

    Finished Project

--- lib/music_player_page.dart
+++ lib/music_player_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:rive/rive.dart';
 
 class MusicPlayerPage extends StatefulWidget {
   const MusicPlayerPage({Key? key}) : super(key: key);
@@ -8,6 +10,68 @@ class MusicPlayerPage extends StatefulWidget {
 }
 
 class _MusicPlayerPageState extends State<MusicPlayerPage> {
+  late RiveAnimationController _prevButtonController;
+  late RiveAnimationController _nextButtonController;
+  late RiveAnimationController _soundWaveController;
+
+  SMIInput<bool>? _playButtonInput;
+  Artboard? _playButtonArtboard;
+
+  void _playTrackChangeAnimation(RiveAnimationController controller) {
+    if (controller.isActive == false) {
+      controller.isActive = true;
+    }
+  }
+
+  void _playPauseButtonAnimation() {
+    if (_playButtonInput?.value == false &&
+        _playButtonInput?.controller.isActive == false) {
+      _playButtonInput?.value = true;
+      _toggleWaveAnimation();
+    } else if (_playButtonInput?.value == true &&
+        _playButtonInput?.controller.isActive == false) {
+      _playButtonInput?.value = false;
+      _toggleWaveAnimation();
+    }
+  }
+
+  void _toggleWaveAnimation() => setState(
+        () => _soundWaveController.isActive = !_soundWaveController.isActive,
+      );
+
+  @override
+  void initState() {
+    super.initState();
+    _prevButtonController = OneShotAnimation(
+      'onPrev',
+      autoplay: false,
+    );
+    _nextButtonController = OneShotAnimation(
+      'onNext',
+      autoplay: false,
+    );
+    _soundWaveController = SimpleAnimation(
+      'loopingAnimation',
+      autoplay: false,
+    );
+
+    rootBundle.load('assets/PlayPauseButton.riv').then((data) {
+      final file = RiveFile.import(data);
+      final artboard = file.mainArtboard;
+      var controller = StateMachineController.fromArtboard(
+        artboard,
+        'PlayPauseButton',
+      );
+      if (controller != null) {
+        artboard.addController(controller);
+        _playButtonInput = controller.findInput('isPlaying');
+      }
+      setState(
+        () => _playButtonArtboard = artboard,
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,6 +91,70 @@ class _MusicPlayerPageState extends State<MusicPlayerPage> {
                 shape: BoxShape.circle,
               ),
             ),
+            SizedBox(
+              height: 60,
+            ),
+            _playButtonArtboard == null
+                ? SizedBox()
+                : Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTapDown: (_) => _playTrackChangeAnimation(
+                          _prevButtonController,
+                        ),
+                        child: SizedBox(
+                          height: 115,
+                          width: 115,
+                          child: RiveAnimation.asset(
+                            'assets/PrevTrackButton.riv',
+                            controllers: [
+                              _prevButtonController,
+                            ],
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTapDown: (_) => _playPauseButtonAnimation(),
+                        child: SizedBox(
+                          height: 125,
+                          width: 125,
+                          child: Rive(
+                            artboard: _playButtonArtboard!,
+                            fit: BoxFit.fitHeight,
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTapDown: (_) => _playTrackChangeAnimation(
+                          _nextButtonController,
+                        ),
+                        child: SizedBox(
+                          height: 115,
+                          width: 115,
+                          child: RiveAnimation.asset(
+                            'assets/NextTrackButton.riv',
+                            controllers: [
+                              _nextButtonController,
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+                height: 100,
+                width: 400,
+                child: RiveAnimation.asset(
+                  'assets/SoundWave.riv',
+                  fit: BoxFit.contain,
+                  controllers: [
+                    _soundWaveController,
+                  ],
+                )),
           ],
         ),
       ),
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg_animations_with_rive_tutorial/music_player_page.dart';
+import 'package:rive/rive.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,9 +11,31 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      Duration(seconds: 3),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (_) => MusicPlayerPage(),
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow[600],
+      body: Center(
+        child: Container(
+          width: 400,
+          child: RiveAnimation.asset(
+            'assets/radioSplashAnimation.riv',
+          ),
+        ),
+      ),
     );
   }
 }
